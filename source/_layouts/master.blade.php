99b41200a82c862a9c9a27d61e7046b031a2839d
rick <rickwestdev@gmail.com>
2018-08-06 22:42:57 +0100

    add talk.to

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -10,5 +10,18 @@
     <body>
         @yield('body')
         <script type="text/javascript" src="{{ mix('js/main.js', 'assets/build') }}"></script>
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/5b68bf18df040c3e9e0c5888/default';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+            })();
+        </script>
+        <!--End of Tawk.to Script-->
     </body>
 </html>
